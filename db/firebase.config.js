ce64ec03ab2d01cfef80c571442e705184421906
alexandrebuisson <38990731+alexandrebuisson@users.noreply.github.com>
2020-06-30 12:13:58 +0200

    Update firebase.config.js

--- db/firebase.config.js
+++ db/firebase.config.js
@@ -3,14 +3,14 @@ require('firebase/auth')
 require('firebase/database')
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyBkPbH5LR7-cXDoE2Lc25XEtgCbvxC5cIU',
-  authDomain: 'bulb-bot.firebaseapp.com',
-  databaseURL: 'https://bulb-bot.firebaseio.com',
-  projectId: 'bulb-bot',
-  storageBucket: 'bulb-bot.appspot.com',
-  messagingSenderId: '463810155573',
-  appId: '1:463810155573:web:8d5329ea6c62ab5915cd7c',
-  measurementId: 'G-D3FE5R9FKF'
+  apiKey: 'token',
+  authDomain: 'authdomain',
+  databaseURL: 'databaseurl',
+  projectId: 'id',
+  storageBucket: 'storageid',
+  messagingSenderId: 'id',
+  appId: 'appid',
+  measurementId: 'id'
 }
 
 firebase.initializeApp(firebaseConfig)
