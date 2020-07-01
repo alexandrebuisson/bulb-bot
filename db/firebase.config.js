43f828616f74196bd03b424a3539b8a35bf7f35d
alexandrebuisson <38990731+alexandrebuisson@users.noreply.github.com>
2020-07-01 11:42:52 +0200

    Update firebase.config.js

--- db/firebase.config.js
+++ db/firebase.config.js
@@ -2,14 +2,7 @@ const firebase = require('firebase')
 require('firebase/database')
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBkPbH5LR7-cXDoE2Lc25XEtgCbvxC5cIU",
-  authDomain: "bulb-bot.firebaseapp.com",
-  databaseURL: "https://bulb-bot.firebaseio.com",
-  projectId: "bulb-bot",
-  storageBucket: "bulb-bot.appspot.com",
-  messagingSenderId: "463810155573",
-  appId: "1:463810155573:web:8d5329ea6c62ab5915cd7c",
-  measurementId: "G-D3FE5R9FKF"
+ ///
 };
 
 firebase.initializeApp(firebaseConfig)
